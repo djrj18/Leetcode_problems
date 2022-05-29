0d379b93ad588cbbb4037cbfb60685fc1edf9645
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-05-29 18:31:31 +0530

    Time: 348 ms (20.44%), Space: 21.7 MB (22.62%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
-        vector<int> v(n);
+        vector<vector<int>> v(n,vector<int>(26,0));
         for(int i = 0;i<n;i++){
-            int d = 0;
             for(int j = 0;j<words[i].length();j++){
-                d = d | (1<<(words[i][j]-'a'));
+                v[i][words[i][j]-'a'] = 1;
             }
-            v[i] = d;
         }
         
         int ans = 0;
     
         for(int i = 0;i < n ; i++){
             for(int j = i+1 ; j < n; j++){
-                if((v[i]&v[j]) == 0){
-                    ans = max(ans,(int)words[i].length() * (int)words[j].length());
+                bool mila= true;
+                for(int k = 0;k<26;k++){
+                    if(v[i][k] == v[j][k] and v[i][k] != 0){
+                        mila = false;
+                        break;
+                    }
                 }
+                if(mila){     ans = max(ans,(int)words[i].length() * (int)words[j].length());}
             }
         }
         return ans;
