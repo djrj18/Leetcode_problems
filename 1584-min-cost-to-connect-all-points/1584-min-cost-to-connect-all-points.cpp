1886d607755afde260b0ed1a3723ab17f3c572a6
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-07-07 20:14:28 +0530

    Time: 806 ms (41.21%), Space: 105.4 MB (30.92%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,42 +1,76 @@
+class DSU{
+    private:
+       vector<int> par;
+       vector<int> sz;
+    public:
+       int size;
+       DSU(int n){
+           par = vector<int>(n);
+           sz = vector<int>(n);
+           for(int i = 0 ; i < n ;i++){
+               par[i] = i;
+               sz[i] = 1;
+           }
+           size = 1;
+       }
+    
+      int FindP(int u){
+          if(par[u] == u){
+              return u;
+          }
+          return par[u] = FindP(par[u]);
+      }
+    
+    void Union(int u,int v){
+        int Pu = FindP(u);
+        int Pv = FindP(v);
+        if(Pu == Pv){
+            return ;
+        }
+        int szU = sz[Pu];
+        int szV = sz[Pv];
+        if(szU > szV){
+            par[Pv] = Pu;
+            sz[Pu] += sz[Pv];
+            size = sz[Pu];
+        }else{
+            par[Pu] = Pv;
+            sz[Pv] += sz[Pu];
+            size = sz[Pv];
+        }
+    }
+};
+
+
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<vector<pair<int,int>>> g(n);
+      priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         for(int i = 0 ; i < n ;i++){
-            for(int j = 0 ; j < n ;j++){
-                if(i != j){
+            for(int j = 1 ; j < n ;j++){
                     int d = abs(points[i][0] - points[j][0])+abs(points[i][1] - points[j][1]);
-                    g[i].push_back({j,d});
-                    g[j].push_back({i,d});
+                    pq.push({d,{i,j}});
                 }
-            }
         }
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        vector<int> visited(n,0),dis(n,INT_MAX);
-        dis[0] = 0;
-        while(pq.size()){
-            pair<int,int> p = pq.top();
-            pq.pop();
-            int d = p.first;
-            int node = p.second;
-            visited[node] = 1;
-            for(auto nb : g[node]){
-                if(!visited[nb.first] and dis[nb.first] > nb.second){
-                    dis[nb.first] = nb.second;
-                    pq.push({nb.second,nb.first});
-                }
-            }
-        }
-        
+        DSU s(n);
         int ans = 0;
-        for(auto d : dis){
-           
-            ans += d;
+        while(pq.size()){
+            pair<int,pair<int,int>> p = pq.top();
+            pq.pop();
+            int u = p.second.first;
+            int v = p.second.second;
+            int Pu = s.FindP(u);
+            int Pv = s.FindP(v);
+            if(Pu != Pv){
+                ans += p.first;
+                s.Union(u,v);
+                if(s.size == n){
+                    return ans;
+                }
+            }
         }
-        
         return ans;
     }
 };
