cba2ac570a414a8fd381fc942bd412335e699370
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-07-22 23:10:10 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (21.04%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<pair<int,char>> star,open;
+        stack<pair<int,char>> star,Open;
         for(int i = 0 ; i < s.length() ;i++){
-            if(s[i] == '('){
-                open.push({i,s[i]});
-            }else if(s[i] == '*'){
-                star.push({i,s[i]});
-            }else{
-                if(!open.empty()){
-                    open.pop();
-                }else if(!star.empty()){
+            char ch = s[i];
+            if(ch == '('){
+                Open.push({i,ch});
+            }
+            else if(ch == '*'){
+                star.push({i,ch});
+            }
+            else{
+                if(Open.size()){
+                    Open.pop();
+                }
+                else if(star.size()){
                     star.pop();
-                }else{
+                }
+                else{
                     return false;
                 }
             }
         }
         
-        while(open.size()){
-            if(star.size() == 0){
-                return false;
-            }
-            if(open.top().first > star.top().first){
-                return false;
-            }
-            open.pop();
-            star.pop();
+        if(Open.size() > star.size()){
+            return false;
         }
         
+        while(Open.size()){
+            if(Open.top().first > star.top().first){
+                return false;
+            }
+            Open.pop();
+            star.pop();
+        }
         return true;
-        
     }
 };
