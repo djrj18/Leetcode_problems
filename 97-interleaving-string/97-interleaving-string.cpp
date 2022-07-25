ac6dea08371138247da81174b1d387161f2bd3c4
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-07-25 22:25:39 +0530

    Time: 98 ms (9.77%), Space: 97.7 MB (5.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
-    bool solve(string s1,string s2,string s3,int i,int j,int k,vector<vector<vector<int>>> &dp){
-        if(i == s1.length() and j == s2.length() and k == s3.length()){
-            return true;
-        }
-        else if(i == s1.length()){
-            while(j < s2.length() and k < s3.length()){
+    bool solve(string s1,string s2, string s3 ,int n,int m,int l ,int i ,int j ,int k,vector<vector<vector<int>>> &dp){
+        if(i == n){
+            while(j < m and k < l){
                 if(s2[j] != s3[k]){
                     return false;
                 }
                 j++;
                 k++;
             }
-            return j == s2.length() and k == s3.length();
-        }else if(j == s2.length()){
-               while(i < s1.length() and k < s3.length()){
+            return j == m and k == l;
+        }
+        if(j == m){
+            while(i < n and k < l){
                 if(s1[i] != s3[k]){
                     return false;
                 }
                 i++;
                 k++;
             }
-            return i == s1.length() and k == s3.length();
-        }else if(k == s3.length()){
+            return i == n and k == l;
+        }
+        if(k == l){
             return false;
         }
         if(dp[i][j][k] != -1){
             return dp[i][j][k];
         }
-        int ans = 0;
-        if(s1[i] == s3[k]){
-            ans |= solve(s1,s2,s3,i+1,j,k+1,dp);
+        if(s1[i] == s3[k] and s3[k] == s2[j]){
+            return dp[i][j][k] = solve(s1,s2,s3,n,m,l,i+1,j,k+1,dp) or solve(s1,s2,s3,n,m,l,i,j+1,k+1,dp);
         }
-        if(s2[j] == s3[k]){
-            ans |= solve(s1,s2,s3,i,j+1,k+1,dp);
+        else if(s1[i] == s3[k]){
+            return dp[i][j][k] = solve(s1,s2,s3,n,m,l,i+1,j,k+1,dp);
+        }
+        else if(s2[j] == s3[k]){
+            return dp[i][j][k] = solve(s1,s2,s3,n,m,l,i,j+1,k+1,dp);
         }
         
-        return dp[i][j][k] = ans;
+       return dp[i][j][k] = false;
     }
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.length();
         int m = s2.length();
         int l = s3.length();
-        if(n+m != l){
-            return false;
-        }
-       vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(l,-1)));
-        return solve(s1,s2,s3,0,0,0,dp);
+         vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(l,-1)));
+        
+        return solve(s1,s2,s3,n,m,l,0,0,0,dp);
     }
 };
