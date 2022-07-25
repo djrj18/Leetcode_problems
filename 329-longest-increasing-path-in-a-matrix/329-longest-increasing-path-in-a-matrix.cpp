161c42412adda9c705debe10c55174bde3aec702
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-07-25 21:59:22 +0530

    Time: 66 ms (55.30%), Space: 16 MB (68.50%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &mat,int i,int j,vector<vector<int>> &dp,int n,int m){
-        if(dp[i][j] != -1){
-            return dp[i][j]; 
-        }
+    int  dfs(vector<vector<int>> &matrix,int i,int j ,vector<vector<int>> &dp,int n,int m){
         
         int di[4] = {-1,1,0,0};
         int dj[4] = {0,0,-1,1};
-        dp[i][j]  = 1;
-        for(int k = 0;k<4;k++){
+        int ans = 0;
+        if(dp[i][j] != 0){
+            return dp[i][j];
+        }
+        for(int k = 0 ; k < 4 ;k++){
             int ni = i+di[k];
             int nj = j+dj[k];
-            if(ni >=0 and ni < n and nj >= 0 and nj < m and mat[ni][nj] > mat[i][j]){
-                dp[i][j] = max(dp[i][j],solve(mat,ni,nj,dp,n,m)+1);
+            if(ni >= 0 and ni < n and nj >= 0 and nj < m and matrix[ni][nj] < matrix[i][j]){
+                ans = max(ans, dfs(matrix,ni,nj,dp,n,m));
             }
         }
-        return dp[i][j];
+        
+        return dp[i][j] = ans+1;
     }
-    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
         int ans = 1;
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(dp[i][j] == -1){
-                    solve(matrix,i,j,dp,n,m);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ;j++){
+                if(dp[i][j] == 0){
+                    dfs(matrix,i,j,dp,n,m);
                 }
                 ans = max(ans,dp[i][j]);
             }
         }
         
         return ans;
+        
         
     }
 };
