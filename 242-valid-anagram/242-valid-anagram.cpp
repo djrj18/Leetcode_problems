7a3e4ba7b517688ec3c27e70014b092cf1859aee
djrj18 <68695065+djrj18@users.noreply.github.com>
2022-07-28 19:14:25 +0530

    Time: 11 ms (70.38%), Space: 7.1 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int> T(26,0);
-        for(auto ch : s){
-            T[ch - 'a']++;
-        }
-        for(auto ch : t){
-            if(T[ch - 'a'] == 0) return false;
-            T[ch - 'a']--;
+        int l = s.length();
+        if(l != t.length()) return false;
+        for(int i = 0 ; i < l ; i++){
+            T[s[i] - 'a']++;
+            T[t[i] - 'a']--;
         }
         
         for(int i = 0 ; i < 26 ;i++){
