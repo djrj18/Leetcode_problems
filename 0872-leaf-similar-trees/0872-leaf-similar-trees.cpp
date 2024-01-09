3e90b64949bf81d1302d10d4cb3e60e8d042d8be
djrj18 <68695065+djrj18@users.noreply.github.com>
2024-01-09 20:51:33 +0530

    Time: 4 ms (29.47%), Space: 13.2 MB (77.45%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,24 +11,40 @@
  */
 class Solution {
 public:
-    void GetLeaves(TreeNode*&root, vector<int> &leaves){
+    bool GetLeaves(TreeNode*& root, vector<int> & leaves, int &ind){
+        if(root->left == NULL and root->right == NULL){
+            if(ind == leaves.size()) return false;
+            else if(leaves[ind] == root->val){
+                ind++;
+                return true;
+            }else{return false;}
+        }else {
+                int left = 1 ;
+                if(root->left) left = GetLeaves(root->left, leaves, ind);
+                int right = 1;
+                if(root->right) right = GetLeaves(root->right, leaves, ind);
+                return (left && right);
+            }
+        
+        
+    }
+    
+    void Func(TreeNode*&root, vector<int> &leaves){
+        if(root == NULL) return;
         if(root->left == NULL and root->right == NULL){
             leaves.push_back(root->val);
             return;
         }
-        
-        if(root->left) GetLeaves(root->left,leaves);
-        if(root->right)GetLeaves(root->right,leaves);
-        
+        Func(root->left, leaves);
+        Func(root->right, leaves);
         return;
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> leaves1, leaves2;
-        GetLeaves(root1, leaves1);
-        GetLeaves(root2, leaves2);
-        
-        return leaves1 == leaves2;
-        
+        vector<int> leaves;
+        Func(root1, leaves);
+        for(auto num : leaves) cout<<num<<" ";
+        int ind = 0;
+        return (GetLeaves(root2, leaves, ind) && (ind == leaves.size()));
         
     }
 };
